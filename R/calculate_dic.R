f4c3dca68a1504d62b47cf0ca9ac14d05c35d76f
Benjamin Trueman <40065050+bentrueman@users.noreply.github.com>
2024-06-03 16:50:59 -0300

    bug fix in calculate_dic(), new tests

--- R/calculate_dic.R
+++ R/calculate_dic.R
@@ -50,6 +50,6 @@ use_method_b <- function(ph, alkalinity, temperature) {
     dic[i] <- tibble::as_tibble(out)$`C(mol/kgw)` * chemr::mass("C") * 1e3
   }
 
-  if (is.na(alkalinity)) NA_real_ else dic
+  ifelse(is.na(alkalinity), NA, dic)
 
 }
--- tests/testthat/test-calculate_dic.R
+++ tests/testthat/test-calculate_dic.R
@@ -6,3 +6,24 @@ test_that("calculate_dic() works", {
 test_that("calculate_dic() returns an error if method is not a or b", {
   expect_error(calculate_dic(7.5, 20, method = "c"))
 })
+
+test_that("calculate_dic() returns NA when alkalinity is NA", {
+  expect_equal(
+    calculate_dic(c(9, 10), c(NA, 19), c(10, 20), method = "b"), c(NA, 2.84669), tolerance = 1e-5
+  )
+  expect_equal(
+    calculate_dic(c(9, 10), c(NA, 19), c(10, 20), method = "a"), c(NA, 2.54765), tolerance = 1e-5
+  )
+})
+
+test_that("calculate_dic() returns NA when pH is NA (method a)", {
+  expect_equal(
+    calculate_dic(c(NA, 10), c(12, 19), c(10, 20), method = "a"), c(NA, 2.54765), tolerance = 1e-5
+  )
+})
+
+test_that("calculate_dic() returns an error when pH or temperature are NA (method b)", {
+  expect_error(calculate_dic(c(NA, 10), c(12, 19), c(10, 20), method = "b"))
+  expect_error(calculate_dic(c(9, 10), c(12, 19), c(NA, 20), method = "b"))
+})
+
