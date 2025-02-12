c3a4c5384277d8c099ea084df807dab7bbe60150
cr-bft <bentrueman@carbonrun.io>
2025-02-12 08:30:46 -0400

    updated test for change_logk()

--- tests/testthat/test-change_logk.R
+++ tests/testthat/test-change_logk.R
@@ -1,6 +1,9 @@
 test_that("change_logk() makes expected modification to a database", {
   logk_new <- 23.999
-  modified <- change_logk(db = phreeqc::minteq.v4.dat, eqn = "Pb+2 + Cl- = PbCl+", logk = logk_new)[1820:1821]
+  modified <- change_logk(db = phreeqc::minteq.v4.dat, eqn = "Pb+2 + Cl- = PbCl+", logk = logk_new)
+  these_rows <- which(stringr::str_detect(modified, "Pb\\+2 \\+ Cl- = PbCl\\+"))
+  these_rows <- c(these_rows, these_rows + 1)
+  modified <- modified[these_rows]
   logk_recovered <- stringr::str_extract(modified[2], "\\d+\\.?\\d+$")
   expect_equal(logk_new, as.numeric(logk_recovered))
 })
@@ -9,14 +12,16 @@ test_that("change_logk() replaces log Ks in default database with those in phree
   rewrite_hydcerussite <- function(x) {
     stringr::str_replace(
       x,
-      modify_eqn("Pb3(OH)2(CO3)2 + 2H+ = 3Pb+2 + 2H2O + 2CO3-2"),
-      "Pb(OH)2:2PbCO3 + 2H+ = 3Pb+2 + 2CO3-2 + 2H2O"
+      modify_eqn("Pb3(OH)2(CO3)2 + 2 H+ = 3 Pb+2 + 2 H2O + 2 CO3-2"),
+      "Pb(OH)2:2PbCO3 + 2 H+ = 3 Pb+2 + 2 CO3-2 + 2 H2O"
     )
   }
   # set up args to change_logk():
   # (these are the log Ks from minteq.v4 that will replace those in the default database)
   db_revision <- pb_logk(db = phreeqc::minteq.v4.dat)
   db_revision$eqn <- rewrite_hydcerussite(db_revision$eqn)
+  # remove spaces between coefficients and elements:
+  db_revision$eqn <- stringr::str_remove_all(db_revision$eqn, "(?<=\\d) (?=[A-Z])")
   remove_these <- c( # not present in minteq.v4.dat
     "6Pb+2 + 8H2O = Pb6(OH)8+4 + 8H+",
     "Pb+2 + PO4-3 + H+ = PbHPO4",
@@ -24,6 +29,8 @@ test_that("change_logk() replaces log Ks in default database with those in phree
   )
   target <- pb_logk(db = phreeqc::minteq.v4.dat)
   target$eqn <- rewrite_hydcerussite(target$eqn)
+  # remove spaces between coefficients and elements:
+  target$eqn <- stringr::str_remove_all(target$eqn, "(?<=\\d) (?=[A-Z])")
   modified <- pb_logk(db = with(db_revision, change_logk(eqn = eqn, logk = log_k)))
   modified <- modified[!modified$eqn %in% remove_these, ]
   expect_equal(target[order(target$log_k), -1], modified[order(modified$log_k), -1])
